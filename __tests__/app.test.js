b05f76b500e88d8f4dcf146bf86d56d625ebf1ea
BrendenSeifried <brendenseifried@gmail.com>
2022-06-23 12:33:36 -0700

    added test for redirecting oauth to github

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,8 +7,11 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('should redirect to github auth page', async () => {
+    const resp = await request(app).get('/api/v1/github/login');
+    expect(resp.header.location).toMatch(
+      /https:\/\/github.com\/login\/oauth\/authorize?client_id=[\w\d]+&scope=user&redirect_url=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
+    );
   });
   afterAll(() => {
     pool.end();
