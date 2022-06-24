739b8fce234ea2a6b4f2f00bd5a6dffd557176f0
BrendenSeifried <brendenseifried@gmail.com>
2022-06-24 11:52:56 -0700

    .delete added to log out

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS git_users;
+DROP TABLE IF EXISTS user_post;
 
 CREATE TABLE git_users(
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -8,3 +9,16 @@ CREATE TABLE git_users(
     email VARCHAR,
     avatar VARCHAR 
 );
+
+CREATE TABLE user_post(
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title VARCHAR NOT NULL,
+    description VARCHAR NOT NULL
+);
+
+INSERT INTO user_post(
+    title,
+    description
+)
+VALUES
+('KNOB KNOBs diary', 'Ate human, taste good');
